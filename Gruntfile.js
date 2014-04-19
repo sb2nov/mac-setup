429e4d12e44a6965cb267b8fc0a4fb0d76344d28
sb2nov <sb2nov@gmail.com>
2014-04-19 02:02:20 -0700

    Update to gitbook - 0.2.3

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-gitbook');
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-http-server');
 
     grunt.initConfig({
         'gitbook': {
             development: {
-                dest: path.join(__dirname, ".grunt/gitbook"),
+                output: path.join(__dirname, ".grunt/gitbook"),
                 input: "./",
                 title: "Mac OS X Setup Guide",
                 description: "Guide to setting up developer environment on OS X",
@@ -23,9 +24,29 @@ module.exports = function (grunt) {
         },
         'clean': {
             files: '.grunt'
+        },
+        'http-server': {
+            'dev': {
+                // the server root directory
+                root: '.grunt/gitbook',
+
+                port: 4000,
+                host: "127.0.0.1",
+
+                showDir : true,
+                autoIndex: true,
+                defaultExt: "html",
+
+                //wait or not for the process to finish
+                runInBackground: false
+            }
         }
     });
 
+    grunt.registerTask('test', [
+        'gitbook',
+        'http-server'
+    ]);
     grunt.registerTask('publish', [
         'gitbook',
         'gh-pages',
