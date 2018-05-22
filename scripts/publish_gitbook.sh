e8d3c948ebfda53f4a1608443930a56fb3c3fd81
Simon Egersand <s.egersand@gmail.com>
2018-05-22 21:34:48 +0200

    Do not deploy if changed files in tree
    I accidentally pushed unwanted changes because I had them in the tree
    when I ran the script to deploy.

--- scripts/publish_gitbook.sh
+++ scripts/publish_gitbook.sh
@@ -2,33 +2,40 @@
 
 readonly commands=(python git gitbook cp)
 
+# Make sure we don't push unrelated changes
+if [[ $(git status -s | wc -l) -gt 0 ]]; then
+  echo "🚨 You have changed files. Aborting."
+  exit 1
+fi
+
 function is_available {
   command -v $1 >/dev/null 2>&1 ||
-    { echo >&2 "I require $1 but it's not installed. Aborting."; exit 1; }
+    { echo >&2 "🚨 I require $1 but it's not installed. Aborting."; exit 1; }
 }
 
 # Make sure all executables are available on $PATH
 for cmd in ${commands[@]}; do is_available "$cmd"; done
+echo "✅ All required packages are available, will continue"
 
-echo "All required packages are available, will continue"
-
-echo "Updating list of contributors"
+echo "👥 Updating list of contributors.."
 python ./scripts/contributors.py
 git commit -a -m "Update list of contributors"
 git push origin master
-echo "Finished updating list of contributors"
+echo "👥 Completed updating list of contributors"
 
-echo "Building the guide using gitbook"
+echo "📖 Building the guide using gitbook.."
 gitbook install && gitbook build
+echo "📖 Done building guide"
 
 git checkout gh-pages
 git pull origin gh-pages --rebase
 
 cp -R _book/* .
 
+echo "🌲 Cleaning untracked files from working tree.."
 git clean -fx node_modules
 git clean -fx _book
-echo "Finished building guide"
+echo "🌲 Done cleaning untracked files"
 
 git add .
 
@@ -38,4 +45,4 @@ git commit -a -m "Deploy version with hash $HASH"
 git push origin gh-pages
 git checkout master
 
-echo "Finished building and deploying new version of guide"
+echo "😎 Finished building and deploying new version of guide"
